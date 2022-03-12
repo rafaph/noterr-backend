e02b95f7b1685a339ebe406a6eb35c04e110ee02
Raphael Castro <rafaphcastro@gmail.com>
2022-03-12 16:38:54 -0300

    test: add create category endpoint

--- test/category/create-category/domain/create-category-use-case.spec.ts
+++ test/category/create-category/domain/create-category-use-case.spec.ts
@@ -2,38 +2,41 @@ import sinon from "sinon";
 import { CreateCategoryRepository } from "@app/category/create-category/domain/create-category-repository";
 import { CreateCategoryUseCase } from "@app/category/create-category/domain/create-category-use-case";
 import { UUID } from "@app/lib/uuid";
-import { makeCreateCategoryRepository, makeCreateCategoryUseCaseInput } from "@test/category/helpers/factories";
+import {
+  makeCreateCategoryRepository,
+  makeCreateCategoryUseCaseInput,
+} from "@test/category/create-category/helpers/factories";
 
 interface Sut {
-  readonly createCategoryRepository: CreateCategoryRepository;
-  readonly createCategoryUseCase: CreateCategoryUseCase;
+  readonly repository: CreateCategoryRepository;
+  readonly useCase: CreateCategoryUseCase;
 }
 
 const makeSut = (createCategoryRepository = makeCreateCategoryRepository()): Sut => ({
-  createCategoryUseCase: new CreateCategoryUseCase(createCategoryRepository),
-  createCategoryRepository,
+  useCase: new CreateCategoryUseCase(createCategoryRepository),
+  repository: createCategoryRepository,
 });
 
 describe("CreateCategoryUseCase", () => {
   it("should return category id on success creation", async () => {
-    const { createCategoryUseCase } = makeSut();
+    const { useCase } = makeSut();
     const input = makeCreateCategoryUseCaseInput();
 
-    const result = await createCategoryUseCase.execute(input);
+    const result = await useCase.execute(input);
 
     expect(result.isRight()).to.be.true;
     expect(result.value).to.be.a.instanceOf(UUID);
   });
 
   it("should return a DefaultError if category already exists", async () => {
-    const { createCategoryUseCase } = makeSut(
+    const { useCase } = makeSut(
       makeCreateCategoryRepository({
         exists: sinon.stub().resolves(true),
       }),
     );
     const input = makeCreateCategoryUseCaseInput();
 
-    const result = await createCategoryUseCase.execute(input);
+    const result = await useCase.execute(input);
 
     expect(result.isLeft()).to.be.true;
     expect(result.value).to.have.keys(["message", "code"]);
